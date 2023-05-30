3e484dc6e43899339575e074af77be23f06bb641
Kevin Murphy <tec.kevinmurphy@gmail.com>
2023-05-30 15:53:07 -0300

    cambie el simulador plazo fijo por un ecommerce

--- js/script.js
+++ js/script.js
@@ -1,66 +1,77 @@
-
-function simulador_pf( capital , plazo) {
-    capital = parseFloat (capital);
-
-    plazo = parseInt (plazo);
-
-    let plazo_fijo = 0;
-
-    if (capital > 0 && plazo == 30) {
-        plazo_fijo = capital + (capital * 0.070);
-        return plazo_fijo
+class Producto {
+    constructor (nombre, precio, stock){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
     }
 
-    else if (capital > 0 && plazo == 90) {
-        plazo_fijo = capital + (capital * 0.20);
-        return plazo_fijo
+    get_datos(){
+        console.log(" ");
+        console.log ("Nombre: ",this.nombre);
+        console.log ("Precio: ",this.precio);
+        console.log ("Stock: ",this.stock);
+        console.log (" ");
     }
 
-    else if (capital > 0 && plazo == 180) {
-        plazo_fijo = capital + (capital * 0.50);
-        return plazo_fijo 
-    }
-
-    else if (capital > 0 && plazo == 365) {
-        plazo_fijo = capital + (capital * 1.15);
-        return plazo_fijo 
-    }
-}
-
-function cliente_nacion (plazo_fijo , usuario) {
-    if (usuario == "SI"){
-        let beneficio_nacion = plazo_fijo + (plazo_fijo * 0.15); 
-        return beneficio_nacion
-    }
-    else {
-        return 0
-    }
-}
-
-
-console.log ("Bienvenido/a a KM-Simulador Plazo Fijo");
-
-let capital = "";
-
-while (capital != "SALIR") {
-    capital = prompt ("Ingrese cuánto desea invertir o ingrese SALIR para finalizar");
-
-    if (capital != "SALIR") {
-        let usuario = prompt ("Es cliente del Banco Nación: SI o NO");
-    
-        let plazo = prompt ("Ingrese el plazo 30,90,180 o 365");
-    
-        let monto_final = simulador_pf (capital , plazo);
-    
-        let monto_beneficio = cliente_nacion (monto_final , usuario);
-    
-        console.log ("Invertiste: " , capital);
-        console.log ("En un plazo de días: " , plazo);
-        console.log ("Vas a recibir: " , monto_final);
-    
-        if (monto_beneficio != 0) {
-            console.log ("Por ser cliente del Banco Nación: " , monto_beneficio);
+    get_stock(){
+        if (this.stock <= 0){
+            return false
+        } 
+        else{
+            return true
         }
-    
     }
+
+    venta_stock (unidades_compra){
+        if (this.stock >= unidades_compra){
+            this.stock = this.stock - unidades_compra;
+            return true
+        }
+        else{
+            return false
+        }
+    }
+}
+
+let carrito = [];
+
+carrito.push (new Producto ("Camiseta Titular",16000,15));
+carrito.push (new Producto ("Camiseta Suplente",15000,20));
+carrito.push (new Producto ("Camiseta Arquero",16000,10));
+carrito.push (new Producto ("Short",8000,12));
+carrito.push (new Producto ("Camiseta Entrenamiento",10000,7));
+carrito.push (new Producto ("Rompevientos",19000,5));
+carrito.push (new Producto ("Jogging",17000,3));
+carrito.push (new Producto ("Medias",2000,17));
+
+console.log ("Lista de Productos");
+
+for (let producto of carrito){
+    producto.get_datos();
+}
+
+function busqueda(producto) {
+    return producto.nombre == compra_usuario
+}
+
+let compra_usuario = prompt ("Ingrese el producto que desea comprar o ingrese SALIR para finalizar");
+let resultado_busqueda =carrito.find (busqueda);
+
+
+if (resultado_busqueda != undefined) {
+    if (resultado_busqueda.get_stock()){
+        let unidades_compra = parseInt (prompt ("¿Cuántas unidades del producto desea comprar?"));
+        if (resultado_busqueda.venta_stock (unidades_compra)) {
+            console.log (`Gracias por comprar ${unidades_compra} unidad/es de ${resultado_busqueda.nombre}`);
+        }
+        else{
+            console.log ("No contamos con stock disponible de: ",resultado_busqueda.nombre);
+            console.log ("Stock disponible: ",resultado_busqueda.stock);
+        }
+    }
+
+    resultado_busqueda.get_datos();
+}
+else{
+    console.log ("No se encontró el producto: ",compra_usuario);
 }
